0bc30c48ad5ebb3763be98b998cd2c76dedb0646
bhoomiparekh313 <bhoomiparekh313@gmail.com>
2024-11-19 21:01:28 +0530

    Add files via upload

---
+++ DAY 2/gdgday2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Enter the number of levels for the pyramid: ";
+    cin >> N;
+
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= N - i; ++j) {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; ++k) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
