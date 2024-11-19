d40334f18c33006fe0b432c0e34086f87feed0fe
bhoomiparekh313 <bhoomiparekh313@gmail.com>
2024-11-19 20:43:30 +0530

    Add files via upload

---
+++ DAY 1/gdgday1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string word;
+    cout << "Enter a single word to be written twice: ";
+    cin >> word;
+    cout << word << " " << word << endl;
+    return 0;
+}
